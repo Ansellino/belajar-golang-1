cbde97e716f09a91ccdb2955cd28937e6dcd924a
AnselZorro <ansellino@gmail.com>
2025-01-02 14:22:17 +0700

    learn struct

--- struct.go
+++ struct.go
@@ -8,5 +8,25 @@ type Customer struct {
 }
 
 func main() {
-	
+	var ansel Customer
+	fmt.Println("Ini sebelum di masukin structnya", ansel)
+
+	ansel.Name = "Jeremy Ansellino Gunawan"
+	ansel.Address = "Indonesia"
+	ansel.Age = 24
+
+	fmt.Println("ini sesudah", ansel)
+	fmt.Println("Ini nama", ansel.Name)
+	fmt.Println("Ini alamat", ansel.Address)
+	fmt.Println("Ini umur", ansel.Age)
+
+	joko := Customer{
+		Name: "Joko",
+		Address: "Indonesia",
+		Age: 30,
+	}
+	fmt.Println(joko)
+
+	budi := Customer{"Budi","Indonesia", 30}
+	fmt.Println(budi)
 }
